eb7e538c34b59b4b666139581b0232ecd405cc59
Brian Le <brian@brianle.xyz>
2026-02-27 09:26:39 -0500

    chore: bump fork version to 0xble.1.1.0

--- version/print.go
+++ version/print.go
@@ -10,7 +10,7 @@ import (
 	"sync"
 )
 
-const forkVersionSuffix = "-0xble"
+const forkVersionSuffix = "-0xble.1.1.0"
 
 func withForkVersionSuffix(v string) string {
 	if strings.HasSuffix(v, forkVersionSuffix) {
